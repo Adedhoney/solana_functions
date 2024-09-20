c01c1275998c15cb7fd01082bc56715de5d9c759
Adedhoney <doyinadeyemi2000@gmail.com>
2024-09-20 22:25:24 +0100

    added bundles to transactions

--- src/transactions/transactions.ts
+++ src/transactions/transactions.ts
@@ -1,85 +1,138 @@
-import * as solana from "@solana/web3.js"
+import {
+    SystemProgram,
+    Connection,
+    ParsedAccountData,
+    Keypair,
+    PublicKey,
+    Transaction,
+    LAMPORTS_PER_SOL,
+    TransactionMessage,
+    VersionedTransaction,
+} from "@solana/web3.js"
+import { sendBundle } from "./Bundle"
+import { searcherClient } from "jito-ts/dist/sdk/block-engine/searcher"
+
+const blockEngineUrl = process.env.BLOCK_ENGINE_URL || ""
 
 // Function to send sol from one account to another
 const sendSolTrasaction = async (
-    connection: solana.Connection,
-    fromKeyPair: solana.Keypair,
-    toKey: solana.PublicKey,
+    connection: Connection,
+    fromKeyPair: Keypair,
+    toKey: PublicKey,
     numOfSol: number
 ) => {
-    const transaction = new solana.Transaction().add(
-        solana.SystemProgram.transfer({
-            fromPubkey: fromKeyPair.publicKey,
-            toPubkey: toKey,
-            lamports: numOfSol * solana.LAMPORTS_PER_SOL,
-        })
-    )
+    const transfer = SystemProgram.transfer({
+        fromPubkey: fromKeyPair.publicKey,
+        toPubkey: toKey,
+        lamports: numOfSol * LAMPORTS_PER_SOL,
+    })
     console.log(fromKeyPair.publicKey.toString())
 
-    const signature = await solana.sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [fromKeyPair]
+    const blockHash = (
+        await connection.getLatestBlockhash("confirmed")
+    ).blockhash
+
+    const messageV0 = new TransactionMessage({
+        payerKey: fromKeyPair.publicKey,
+        recentBlockhash: blockHash,
+        instructions: [transfer],
+    }).compileToV0Message()
+
+    const transaction = new VersionedTransaction(messageV0)
+    transaction.sign([fromKeyPair])
+
+    const sc = searcherClient(blockEngineUrl, fromKeyPair)
+    const bundleTransactionLimit = parseInt(
+        process.env.BUNDLE_TRANSACTION_LIMIT || "5"
     )
-    console.log("SIGNATURE", signature)
+
+    await sendBundle(
+        sc,
+        [transaction],
+        bundleTransactionLimit,
+        connection,
+        fromKeyPair
+    )
 }
 // Function to request sol to a particular account: Remember to use clusterURL("devnet") in connection before you request airdrop
 const requestAirdrop = async (
-    connection: solana.Connection,
-    publicKey: solana.PublicKey
+    connection: Connection,
+    publicKey: PublicKey
 ) => {
-    await connection.requestAirdrop(publicKey, 1 * solana.LAMPORTS_PER_SOL)
+    await connection.requestAirdrop(
+        publicKey,
+        1 * LAMPORTS_PER_SOL
+    )
 }
 
 // Function to send tokens from one account to another
 
 const sendSplToken = async (
-    connection: solana.Connection,
-    fromKeyPair: solana.Keypair,
-    toKey: solana.PublicKey,
+    connection: Connection,
+    fromKeyPair: Keypair,
+    toKey: PublicKey,
     numOfToken: number
 ) => {
     const splToken = await import("@solana/spl-token")
     // Change mint address to that of your token
-    const mintAddress = new solana.PublicKey(
+    const mintAddress = new PublicKey(
         "4JeGKgkrwcNJMESbJp5KrnQVmQdEffwr7zhicijz2BgC"
     )
-    const accountInfo = await connection.getParsedAccountInfo(mintAddress)
-    const tokenDecimals = (accountInfo.value?.data as solana.ParsedAccountData)
-        .parsed.info.decimals as number
-    const tokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
-        connection,
-        fromKeyPair,
-        mintAddress,
-        fromKeyPair.publicKey
-    )
-    const destinationAccount = await splToken.getOrCreateAssociatedTokenAccount(
-        connection,
-        fromKeyPair,
-        mintAddress,
-        toKey
-    )
-    const transaction = new solana.Transaction().add(
-        splToken.createTransferInstruction(
-            tokenAccount.address,
-            destinationAccount.address,
-            fromKeyPair.publicKey,
-            numOfToken * Math.pow(10, tokenDecimals)
+    const accountInfo =
+        await connection.getParsedAccountInfo(mintAddress)
+    const tokenDecimals = (
+        accountInfo.value?.data as ParsedAccountData
+    ).parsed.info.decimals as number
+    const tokenAccount =
+        await splToken.getOrCreateAssociatedTokenAccount(
+            connection,
+            fromKeyPair,
+            mintAddress,
+            fromKeyPair.publicKey
         )
+    const destinationAccount =
+        await splToken.getOrCreateAssociatedTokenAccount(
+            connection,
+            fromKeyPair,
+            mintAddress,
+            toKey
+        )
+    const transfer = splToken.createTransferInstruction(
+        tokenAccount.address,
+        destinationAccount.address,
+        fromKeyPair.publicKey,
+        numOfToken * Math.pow(10, tokenDecimals)
     )
-    const latestBlockHash = await connection.getLatestBlockhash("confirmed")
-    transaction.recentBlockhash = await latestBlockHash.blockhash
-    const signature = await solana.sendAndConfirmTransaction(
+    const blockHash = (
+        await connection.getLatestBlockhash("confirmed")
+    ).blockhash
+
+    const messageV0 = new TransactionMessage({
+        payerKey: fromKeyPair.publicKey,
+        recentBlockhash: blockHash,
+        instructions: [transfer],
+    }).compileToV0Message()
+
+    const transaction = new VersionedTransaction(messageV0)
+    transaction.sign([fromKeyPair])
+
+    const sc = searcherClient(blockEngineUrl, fromKeyPair)
+    const bundleTransactionLimit = parseInt(
+        process.env.BUNDLE_TRANSACTION_LIMIT || "5"
+    )
+
+    await sendBundle(
+        sc,
+        [transaction],
+        bundleTransactionLimit,
         connection,
-        transaction,
-        [fromKeyPair]
+        fromKeyPair
     )
-    console.log("successful")
 }
 
 const mintToken = async (
-    connection: solana.Connection,
-    fromKeyPair: solana.Keypair
+    connection: Connection,
+    fromKeyPair: Keypair
 ) => {
     const splToken = await import("@solana/spl-token")
     console.log("this is here")
@@ -95,12 +148,13 @@ const mintToken = async (
     )
 
     console.log(1)
-    const tokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
-        connection,
-        fromKeyPair,
-        mint,
-        fromKeyPair.publicKey
-    )
+    const tokenAccount =
+        await splToken.getOrCreateAssociatedTokenAccount(
+            connection,
+            fromKeyPair,
+            mint,
+            fromKeyPair.publicKey
+        )
 
     console.log(tokenAccount)
     await splToken.mintTo(
@@ -114,4 +168,9 @@ const mintToken = async (
     // await token.mintTo
 }
 
-export { sendSolTrasaction, requestAirdrop, sendSplToken, mintToken }
+export {
+    sendSolTrasaction,
+    requestAirdrop,
+    sendSplToken,
+    mintToken,
+}
